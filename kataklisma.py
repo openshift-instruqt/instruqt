2016ebe5c4daf4aa164249f86e9c114bbd0aa12b
Natale Vinto <nvinto@redhat.com>
2021-10-03 19:57:56 +0200

    Add bulk convert with git submodules

--- kataklisma.py
+++ kataklisma.py
@@ -2,95 +2,154 @@
 
 # Katacoda to Instruqt converter
 
+
+# v. 0.0.2
+# Support for bulk migrate with git submodule
+#
 # v. 0.0.1
+# First draft
+#
 
 import os
 import json
 import yaml
 import re
+import shutil
+
 
 track_d={}
 
-with open('../using-the-cluster-pathway.json', 'r') as myfile:
-    data=myfile.read()
+with open('learn-katacoda//homepage-pathway.json', 'r') as hfile:
+    hdata=hfile.read()
 
-courses = json.loads(data)
+hcourses = json.loads(hdata)
 
-for course in courses['courses']:
-    pathway_id=course['pathway_id']
-    course_id=course['course_id']
-    title=course['title']
+for course in hcourses['courses']:
+    path=re.match('https\:\/\/learn\.openshift\.com\/(.*)\/', course["external_link"] )
+    pathway=path.group(1)
+    print("Creating or updating Topic : " + pathway)
     
-    trackDir=course_id
-    
-    
-    track_d["title"] = title
-    track_d["slug"] = course_id
-    track_d["type"] = "truck"
-    
-    if not os.path.exists(trackDir):
-      os.mkdir(trackDir)
-      print("Directory " , trackDir ,  " Created ")
+    if not os.path.exists(pathway):
+      os.mkdir(pathway)
+      print("Directory " , pathway ,  " Created ")
     else:    
-      print("Directory " , trackDir ,  " already exists")
-      
-    with open('../' + pathway_id + '/' + course_id + '/index.json', 'r') as mycourse:
-        course_data=mycourse.read()
-    
-    course_json = json.loads(course_data)
-    
-    track_d["icon"] = "https://logodix.com/logo/1910931.png"
-    track_d["level"] = "beginner"
-    track_d["tags"] = ["openshift"]
-    track_d["owner"] = ["openshift"]
-    track_d["developers"] = [ "btannous@redhat.com", "nvinto@redhat.com","rjarvine@redhat.com"]
-    track_d["private"] =  "false"
-    track_d["published"] = "true"
-    track_d["skipping_enabled"] = "true"
-    
-    difficulty="intermediate"
-    # course_json["time"]
-    time="300"
-    
-    if course_json["difficulty"] == "beginner":
-      difficulty="basic"
+      print("Directory " , pathway ,  " already exists")
   
+    print("Reading pathway " + 'learn-katacoda/' + pathway + '-pathway.json')
+    with open('learn-katacoda/' + pathway + '-pathway.json', 'r') as myfile:
+        data=myfile.read()
+
+    courses = json.loads(data)
+
+    for course in courses['courses']:
+        pathway_id=''
+        if "pathway_id" in course:
+          pathway_id=course['pathway_id']
+        else:
+          pathway_id=pathway
         
-    l_challenges=[]
-    d_challenges={}
-    
-    for step in course_json["details"]["steps"]:
-        slug = step["text"]
-        slug = re.sub(r'\.md$', '', slug )
-        d_challenges["slug"] = slug
-        d_challenges["title"] = step["title"]
-        d_challenges["type"] = "challenge"
+        course_id=course['course_id']
+        title=course['title']
         
-        with open('../' + pathway_id + '/' + course_id + '/' + step["text"], 'r') as myassign:
-            assign_data=myassign.read()
+        trackDir=course_id
         
-        md=re.sub(r'`{1,2}(.*)`{1,3}\{\{execute\}\}', r'```\n\1\n```', assign_data )
-        md=re.sub(r'\{\{copy\}\}',r'', md)
-        md=re.sub(r'\(\.\.\/\.\.\/assets',r'(https://katacoda.com/openshift/assets',md)
         
-        d_challenges["assignment"] =  md
+        track_d["title"] = title
+        track_d["slug"] = course_id
+        track_d["type"] = "truck"
         
-        d_challenges["tabs"] = [{"title": "cli", "type": "terminal","hostname":"crc-nonest-1"},
-                                {"title": "web-ui", "type" : "service", "hostname" : "crc-nonest-1", "port" : "30443"}]
+        if not os.path.exists(pathway + '/' + trackDir):
+          os.mkdir(pathway + '/' + trackDir)
+          print("Directory " , pathway + '/' + trackDir ,  " Created ")
+        else:    
+          print("Directory " , pathway + '/' + trackDir ,  " already exists")
         
-        d_challenges["difficulty"]= difficulty
-        d_challenges["timelimit"]= time
         
-        dictionary_copy = d_challenges. copy()
-        l_challenges.append(dictionary_copy);
+        try:
+          with open('learn-katacoda/' + pathway_id + '/' + course_id + '/index.json', 'r') as mycourse:
+              course_data=mycourse.read()
+        except FileNotFoundError:
+          print("Path " + 'learn-katacoda/' + pathway_id + '/' + course_id + '/index.json' + " not found, skipping")
+          continue
         
+        course_json = json.loads(course_data)
+        
+        track_d["icon"] = "https://logodix.com/logo/1910931.png"
+        
+        track_d["tags"] = ["openshift"]
+        track_d["owner"] = ["openshift"]
+        track_d["developers"] = [ "btannous@redhat.com", "nvinto@redhat.com","rjarvine@redhat.com"]
+        track_d["private"] =  "false"
+        track_d["published"] = "true"
+        track_d["skipping_enabled"] = "true"
+        
+        difficulty="intermediate"
+        level="beginner"
+        # course_json["time"]
+        time="300"
+        
+        if course_json["difficulty"] == "beginner":
+          difficulty="basic"
+          level="beginner"
+        
+        track_d["level"] = level
+      
+            
+        l_challenges=[]
+        d_challenges={}
+        
+        src=r'config.yml'
+        dst= pathway + '/' + trackDir + '/' + 'config.yml'
+        shutil.copyfile(src, dst)
+        
+        shutil.copytree('track_scripts', pathway + '/' + trackDir + '/track_scripts')
+        
+        
+        for step in course_json["details"]["steps"]:
+            slug = step["text"]
+            
+            slug = re.sub(r'\.md$', '', slug )
+            
+            if not os.path.exists(pathway + '/' + trackDir + '/' + slug):
+              os.mkdir(pathway + '/' + trackDir + '/' + slug)
+              print("Directory " , pathway + '/' + trackDir + '/' + slug ,  " Created ")
+            else:    
+              print("Directory " , pathway + '/' + trackDir + '/' + slug ,  " already exists")
+            
 
 
-    
-    track_d["challenges"] = l_challenges
-    
-    with open(trackDir + '/track.yml', 'w') as yaml_file:
-      yaml.dump(track_d, yaml_file, default_flow_style=False)
+
+            
+            
+            d_challenges["slug"] = slug
+            d_challenges["title"] = step["title"]
+            d_challenges["type"] = "challenge"
+            
+            with open('learn-katacoda/' + pathway_id + '/' + course_id + '/' + step["text"], 'r') as myassign:
+                assign_data=myassign.read()
+            
+            md=re.sub(r'`{1,3}(.*)`{1,3}\{\{execute\}\}', r'```\n\1\n```', assign_data )
+            md=re.sub(r'\{\{copy\}\}',r'', md)
+            md=re.sub(r'\(\.\.\/\.\.\/assets',r'(https://katacoda.com/openshift/assets',md)
+            
+            d_challenges["assignment"] =  md
+            
+            d_challenges["tabs"] = [{"title": "cli", "type": "terminal","hostname":"crc-nonest-1"},
+                                    {"title": "web-ui", "type" : "service", "hostname" : "crc-nonest-1", "port" : "30443"}]
+            
+            d_challenges["difficulty"]= difficulty
+            d_challenges["timelimit"]= time
+            
+            dictionary_copy = d_challenges. copy()
+            l_challenges.append(dictionary_copy);
+            
+
+
+        
+        track_d["challenges"] = l_challenges
+        
+        with open(pathway + '/' + trackDir + '/track.yml', 'w') as yaml_file:
+          yaml.dump(track_d, yaml_file, default_flow_style=False)
 
 
 
