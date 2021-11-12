5c3814b5c6a881426f8aefac0aa5b276421bf4d3
Natale Vinto <nvinto@redhat.com>
2021-11-12 19:46:31 +0100

    Fix script

--- scripts/install_java_graalvm.sh
+++ scripts/install_java_graalvm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p /opt/java
 curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-19.3.6/graalvm-ce-java11-linux-amd64-19.3.6.tar.gz -o /tmp/graalvm-ce-java11-linux-amd64-19.3.6.tar.gz
-tar -xvf /tmp/graalvm-ce-java11-linux-amd64-19.3.6.tar.gz-C /opt/java/
+tar -xvf /tmp/graalvm-ce-java11-linux-amd64-19.3.6.tar.gz -C /opt/java/
 rm -fr /tmp/graalvm-ce-java11-linux-amd64-19.3.6.tar.gz
 
 curl -s https://dlcdn.apache.org/maven/maven-3/3.8.3/binaries/apache-maven-3.8.3-bin.tar.gz -o /tmp/apache-maven-3.8.3-bin.tar.gz
