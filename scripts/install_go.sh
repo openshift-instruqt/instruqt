4a63c4d8df487d0ec34dc88f488c3be323788ff2
Natale Vinto <nvinto@redhat.com>
2021-11-17 18:59:13 +0100

    Use GO version compatible with labs

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 
-curl -s https://dl.google.com/go/go1.17.3.linux-amd64.tar.gz -o /tmp/go1.17.3.linux-amd64.tar.gz 
+curl -s https://dl.google.com/go/go1.16.3.linux-amd64.tar.gz -o /tmp/go1.16.3.linux-amd64.tar.gz 
 
-tar -xvf /tmp/go1.17.3.linux-amd64.tar.gz -C /usr/local/
-rm -f /tmp/go1.17.3.linux-amd64.tar.gz
+tar -xvf /tmp/go1.16.3.linux-amd64.tar.gz -C /usr/local/
+rm -f /tmp/go1.16.3.linux-amd64.tar.gz
 
-echo 'export GOPATH=/usr/local/go' >> /root/.bashrc
-echo 'export PATH=$GOPATH/bin:$PATH' >> /root/.bashrc
+mkdir -p /root/tutorial/go
+
+echo 'export GOROOT=/usr/local/go' >> /root/.bashrc
+echo 'export GOPATH=/root/tutorial/go/' >> /root/.bashrc
+echo 'export GOBIN=/root/tutorial/go/bin' >> /root/.bashrc
+echo 'export PATH=$GOROOT/bin:$PATH' >> /root/.bashrc
