2636b8d0f58c05cf3cb7f9fff0e7f1449217163b
Natale Vinto <nvinto@redhat.com>
2021-11-18 15:16:45 +0100

    Update install_operatorsdk_toolbox.sh

--- scripts/install_operatorsdk_toolbox.sh
+++ scripts/install_operatorsdk_toolbox.sh
@@ -3,6 +3,8 @@ curl -s https://raw.githubusercontent.com/openshift-instruqt/instruqt/master/scr
 
 source /root/.bashrc
 
+mkdir -p /root/tutorial/go/bin
+
 # Download dep
 curl -s https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
 
@@ -14,10 +16,3 @@ export OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/re
 
 curl -sL ${OPERATOR_SDK_DL_URL}/operator-sdk_${OS}_${ARCH} -o /usr/local/bin/operator-sdk
 chmod +x /usr/local/bin/operator-sdk
-
-
-# Download local assets
-
-curl -s https://raw.githubusercontent.com/openshift-instruqt/instruqt/master/operatorframework/go-operator-podset/scripts/podset_types.go -o /tmp/podset_types.go 
-curl -s https://raw.githubusercontent.com/openshift-instruqt/instruqt/master/operatorframework/go-operator-podset/scripts/podset_controller.go -o /tmp/podset_types.go
-curl -s https://raw.githubusercontent.com/openshift-instruqt/instruqt/master/operatorframework/go-operator-podset/scripts/app_v1alpha1_podset.yaml -o /tmp/app_v1alpha1_podset.yaml
