ca489398394f11d84cc7d2f94b635f375afe55f1
Daniel Oh <doh@redhat.com>
2023-02-14 10:48:18 -0500

    Update install_java.sh

--- scripts/install_java.sh
+++ scripts/install_java.sh
@@ -3,8 +3,9 @@
 export MVN_VERSION=3.8.6
 
 mkdir -p /opt/java
-curl -s https://download.java.net/java/GA/jdk17/0d483333a00540d886896bac774ff48b/35/GPL/openjdk-17_linux-x64_bin.tar.gz -o /tmp/openjdk-17_linux-x64_bin.tar.gz
-tar -xvf /tmp/openjdk-17_linux-x64_bin.tar.gz -C /opt/java/
+
+curl -LJO https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.6+10/OpenJDK17U-jdk_x64_linux_hotspot_17.0.6_10.tar.gz /tmp/openjdk-17_linux-x64_bin.tar.gz
+tar -xzvf /tmp/openjdk-17_linux-x64_bin.tar.gz -C /opt/java/
 rm -f /tmp/openjdk-17_linux-x64_bin.tar.gz
 
 curl -s https://archive.apache.org/dist/maven/maven-3/${MVN_VERSION}/binaries/apache-maven-${MVN_VERSION}-bin.tar.gz -o /tmp/apache-maven-${MVN_VERSION}-bin.tar.gz
@@ -12,6 +13,6 @@ curl -s https://archive.apache.org/dist/maven/maven-3/${MVN_VERSION}/binaries/ap
 tar -xzvf /tmp/apache-maven-${MVN_VERSION}-bin.tar.gz -C /opt/java/
 rm -fr /tmp/apache-maven-${MVN_VERSION}-bin.tar.gz
 
-echo 'export JAVA_HOME=/opt/java/jdk-11/' >> /root/.bashrc
+echo 'export JAVA_HOME=/opt/java/jdk-17.0.6+10/' >> /root/.bashrc
 echo "export MAVEN_HOME=/opt/java/apache-maven-${MVN_VERSION}/" >> /root/.bashrc
 echo 'export PATH="$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH"'  >> /root/.bashrc
